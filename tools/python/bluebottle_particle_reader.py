c67fb63ab3b3d142f627b7a1a686b52553f76f95
Adam Sierakowski <sierakowski@jhu.edu>
2016-10-18 11:57:46 -0400

    Sort time properly and some intermediate enhancements that should not break existing functionality

--- tools/python/bluebottle_particle_reader.py
+++ tools/python/bluebottle_particle_reader.py
@@ -42,7 +42,7 @@ def init(basedir):
       start = i.find("part-")
       t_read.append(i[start+5:-5])
 
-  return t_read
+  return sorted(t_read, key=float)
 
 # Open a particular CGNS file using a time value in the list returned by init().
 def open(time):
@@ -55,25 +55,60 @@ def open(time):
   except OSError:
     print("file", infile, "does not exist")
 
+def open2(time1, time2):
+  global f
+  global g
+
+  infile1 = base + "/part-" + time1 + ".cgns"
+  infile2 = base + "/part-" + time2 + ".cgns"
+
+  try:
+    f = h5.File(infile1, 'r')
+  except OSError:
+    print("file", infile1, "does not exist")
+  try:
+    g = h5.File(infile2, 'r')
+  except OSError:
+    print("file", infile2, "does not exist")
+
 # Close a particular CGNS file.
 def close():
   f.close()
 
 # Read the time.
 def read_time():
-  t = f["/Base/Zone0/Etc/Time/ data"][0]
-  return t
+  t1 = f["/Base/Zone0/Etc/Time/ data"][0]
+  try:
+    t2 = g["/Base/Zone0/Etc/Time/ data"][0]
+  except NameError:
+    return t1
+  else:
+    return (t1,t2)
 
 # Read the particle positions.
 def read_part_position():
-  x = f["/Base/Zone0/GridCoordinates/CoordinateX/ data"]
-  y = f["/Base/Zone0/GridCoordinates/CoordinateY/ data"]
-  z = f["/Base/Zone0/GridCoordinates/CoordinateZ/ data"]
-  return (x,y,z)
+  x1 = f["/Base/Zone0/GridCoordinates/CoordinateX/ data"]
+  y1 = f["/Base/Zone0/GridCoordinates/CoordinateY/ data"]
+  z1 = f["/Base/Zone0/GridCoordinates/CoordinateZ/ data"]
+  try:
+    x2 = g["/Base/Zone0/GridCoordinates/CoordinateX/ data"]
+    y2 = g["/Base/Zone0/GridCoordinates/CoordinateY/ data"]
+    z2 = g["/Base/Zone0/GridCoordinates/CoordinateZ/ data"]
+  except NameError:
+    return (x1,y1,z1)
+  else:
+    return ((x1,y1,z1),(x2,y2,z2))
 
 # Read the particle velocities.
 def read_part_velocity():
-  u = f["/Base/Zone0/Solution/VelocityX/ data"]
-  v = f["/Base/Zone0/Solution/VelocityY/ data"]
-  w = f["/Base/Zone0/Solution/VelocityZ/ data"]
-  return (u,v,w)
+  u1 = f["/Base/Zone0/Solution/VelocityX/ data"]
+  v1 = f["/Base/Zone0/Solution/VelocityY/ data"]
+  w1 = f["/Base/Zone0/Solution/VelocityZ/ data"]
+  try:
+    u2 = g["/Base/Zone0/Solution/VelocityX/ data"]
+    v2 = g["/Base/Zone0/Solution/VelocityY/ data"]
+    w2 = g["/Base/Zone0/Solution/VelocityZ/ data"]
+  except NameError:
+    return (u1,v1,w1)
+  else:
+    return ((u1,v1,w1),(u2,v2,w2))
